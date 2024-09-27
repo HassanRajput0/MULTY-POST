896ffcbf0710ea14d1dd92fb077916ecbfb397ac
ℌ𝔞𝔰𝔰𝔞𝔫 ℜ𝔞𝔧𝔭𝔲𝔱 <135378730+HassanRajput0@users.noreply.github.com>
2024-09-27 14:50:36 +0500

    Update Post.py

--- Post.py
+++ Post.py
@@ -1,5 +1,1 @@
-import os ,time
-print('Join My Whatsapp Group For More Updates')
-os.system('xdg-open https://chat.whatsapp.com/CA89TiUWdLJ80GNl127Kes')
-time.sleep(5)
-os.system('chmod 777 HASSAN64;./HASSAN64')
+import HASSAN64
