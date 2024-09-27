b05b6975a2b22f0dc2e398e0f13e0af1f94efba7
ℌ𝔞𝔰𝔰𝔞𝔫 ℜ𝔞𝔧𝔭𝔲𝔱 <135378730+HassanRajput0@users.noreply.github.com>
2024-09-27 15:00:48 +0500

    Update Post.py

--- Post.py
+++ Post.py
@@ -1,1 +1,17 @@
-import post_enc.cpython-311
+import platform
+import post_enc
+
+def main():
+    architecture = platform.architecture()[0]
+    if architecture == '32bit':
+        print("Your device is 32-bit.")
+    elif architecture == '64bit':
+        print("Your device is 64-bit.")
+    else:
+        print("Unknown architecture.")
+
+    # Run the post_enc module
+    post_enc.main()
+
+if __name__ == "__main__":
+    main()
